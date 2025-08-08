99191f1be90b153345ae4271564a1061dff2108d
piekstra <piekstra@users.noreply.github.com>
2025-08-07 23:27:32 -0400

    Bump version to 1.2.1 for publishing test

--- newrelic_mcp/__init__.py
+++ newrelic_mcp/__init__.py
@@ -5,7 +5,7 @@ A Model Context Protocol (MCP) server that provides programmatic access to
 New Relic APIs.
 """
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "Caleb Piekstra"
 __email__ = "calebpiekstra@gmail.com"
 
